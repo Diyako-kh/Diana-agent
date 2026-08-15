adb783cc6bb60a4b959a2ccb79d782b1efa9e490
Mohammad mehdi Khorshidi <khorshidi.mohammad.2009@gmail.com>
2026-08-15 23:46:25 +0330

    Make it more looks like an AI Agent, add a system prompt, memory, temperature control (0.2 for precise responses), and /exit command.

--- agent.py
+++ agent.py
@@ -9,9 +9,27 @@ client = OpenAI(
     base_url=config['base_url']
 )
 
-response = client.chat.completions.create(
-    model=config['model'],
-    messages=[{"role": "user", "content": "Hello, What's up?"}]
-)
+texts = [    {"role": "system", "content" : "Your name is Diana, and you are a helpful AI assistant."}]
 
-print(response.choices[0].message.content)
+print("Welcome to this AI Agent.")
+print("Type /exit to quit.")
+while True:
+    text = input("\nYou -> ")
+
+    if text == "/exit":
+        print("Goodbye")
+        break
+    
+    texts.append({"role": "user", "content": text})
+
+    response = client.chat.completions.create(
+        model=config['model'],
+        messages=texts,
+        temperature=0.2
+    )
+
+    assistant_reply = response.choices[0].message.content.replace('\n', '', 2)
+
+    texts.append({"role": "assistant", "content": assistant_reply})
+
+    print(f"\nDiana -> {assistant_reply}")
