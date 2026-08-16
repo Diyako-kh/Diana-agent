474423b988172b8b0cc02f96fad907a6436d90aa
Mohammad mehdi Khorshidi <khorshidi.mohammad.2009@gmail.com>
2026-08-16 17:05:35 +0330

    Added error handling, /new command to clear the memory and start a new session, /save to save the conversation to a text file(last 20 messages for now but I'll fix that later), token usage tracking, limited conversation to last 20 messages(10 for user and 10 for agent), used colorama in some cases, I'll work more for the appearance later; also added colorama to requirements.txt file.

--- agent.py
+++ agent.py
@@ -1,5 +1,11 @@
 import json
+import datetime
+from os import system
 from openai import OpenAI
+from colorama import Fore, init, Style
+
+
+init()
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -9,27 +15,57 @@ client = OpenAI(
     base_url=config['base_url']
 )
 
-texts = [    {"role": "system", "content" : "Your name is Diana, and you are a helpful AI assistant."}]
+texts = [{"role": "system", "content" : "Your name is Diana."}]
 
 print("Welcome to this AI Agent.")
-print("Type /exit to quit.")
+print("Type /exit to quit.\n")
+print("Token used: 0")
+
 while True:
-    text = input("\nYou -> ")
+    try:
+        text = input(Fore.CYAN + "You -> " + Style.RESET_ALL)
+
+    except KeyboardInterrupt:
+        print("\nGoodbye")
+        exit()
 
     if text == "/exit":
         print("Goodbye")
         break
-    
+
+    elif text == "/new":
+        texts = [{"role": "system", "content" : "Your name is Diana."}]
+        print("\nConversation cleared")
+        continue
+    elif text == "/save":
+        conversation_file = f"chat_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+        with open(conversation_file, "w") as f:
+            for msg in texts:
+                if msg['role'] != 'system':
+                    f.write(f"{msg['role']} : {msg['content']}\n")
+
+        print(f"Conversation saved in : {conversation_file}")
+        continue
+
     texts.append({"role": "user", "content": text})
+    
+    try:
+        response = client.chat.completions.create(
+            model=config['model'],
+            messages=texts,
+            temperature=0.2
+        )
 
-    response = client.chat.completions.create(
-        model=config['model'],
-        messages=texts,
-        temperature=0.2
-    )
+        assistant_reply = response.choices[0].message.content.replace('\n', '', 2)
+        
+        if len(text) > 21:
+            texts =  [texts[0]] + texts[-20:]
 
-    assistant_reply = response.choices[0].message.content.replace('\n', '', 2)
+        texts.append({"role": "assistant", "content": assistant_reply})
 
-    texts.append({"role": "assistant", "content": assistant_reply})
+        print(Fore.YELLOW + f"\nDiana -> {Style.RESET_ALL + assistant_reply}")
+        print(f"\nToken used: {response.usage.total_tokens}")
 
-    print(f"\nDiana -> {assistant_reply}")
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+        break
